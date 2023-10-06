ffc103310a7e19beebd1b6532bbec3e1f9a3a10b
justinablair <justinablair3@icloud.com>
2023-10-06 16:47:04 +0100

    info modal

--- components/theme/modals/InfoModal.tsx
+++ components/theme/modals/InfoModal.tsx
@@ -31,19 +31,23 @@ const InfoModal: React.FC<InfoModalProps> = ({
 }) => {
   return (
     <Modal visible={visible} animationType="slide" transparent>
-      <View style={styles.modalContainer}>
-        <View style={[styles.modalContent, contentStyle]}>
-          <TouchableOpacity
-            style={styles.modalCloseIcon}
-            onPress={onPressClose}>
-            <Image
-              source={require('../../../assets/Close.png')}
+      <View style={styles.modalBackground}>
+        {/* Background with opacity */}
+        <View>
+          {/* Modal Content */}
+          <View style={[styles.modalContent, contentStyle]}>
+            <TouchableOpacity
               style={styles.modalCloseIcon}
-              accessibilityLabel={accessibilityLabel}
-            />
-          </TouchableOpacity>
-          <Text style={[styles.modalTitle, titleStyle]}>{title}</Text>
-          <Text style={[styles.bodyText, bodyTextStyle]}>{content}</Text>
+              onPress={onPressClose}>
+              <Image
+                source={require('../../../assets/Close.png')}
+                style={styles.modalCloseIcon}
+                accessibilityLabel={accessibilityLabel}
+              />
+            </TouchableOpacity>
+            <Text style={[styles.modalTitle, titleStyle]}>{title}</Text>
+            <Text style={[styles.bodyText, bodyTextStyle]}>{content}</Text>
+          </View>
         </View>
       </View>
     </Modal>
@@ -51,16 +55,17 @@ const InfoModal: React.FC<InfoModalProps> = ({
 };
 
 const styles = StyleSheet.create({
-  modalContainer: {
+  modalBackground: {
     flex: 1,
-    backgroundColor: 'rgba(0, 0, 0, 0.5)', // Semi-transparent background
+    backgroundColor: 'rgba(0, 0, 0, 0.5)',
     justifyContent: 'center',
     alignItems: 'center',
   },
+
   modalContent: {
     backgroundColor: '#454949',
     width: '90%',
-    maxHeight: '90%', // Set maxHeight to allow expansion based on content
+    maxHeight: '100%', // Set maxHeight to allow expansion based on content
     borderRadius: 8,
     padding: 20,
     paddingTop: 50, // Add more padding at the top for the Close.png icon
