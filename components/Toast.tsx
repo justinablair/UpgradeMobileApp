e632ee6667fa053ac18ec358ccbb96a4f0a35a95
justinablair <justinablair3@icloud.com>
2023-10-07 16:58:51 +0100

    improved styling of toast

--- components/Toast.tsx
+++ components/Toast.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {View, StyleSheet} from 'react-native';
+import {View, StyleSheet, Image} from 'react-native';
 import Text from './Text';
 import Colour from './theme/Colour';
 
@@ -10,7 +10,13 @@ interface ToastProps {
 const Toast: React.FC<ToastProps> = ({message}) => {
   return (
     <View style={styles.toastContainer}>
-      <Text variant="bodyText" style={{color: Colour.white}}>
+      <View style={styles.imageContainer}>
+        <Image
+          source={require('../assets/Warning.png')}
+          accessibilityLabel="Exclamation mark"
+        />
+      </View>
+      <Text variant="bodyText" style={[{color: Colour.white}, styles.text]}>
         {message}
       </Text>
     </View>
@@ -21,7 +27,18 @@ const styles = StyleSheet.create({
   toastContainer: {
     backgroundColor: 'black',
     width: '100%',
-    height: 50,
+    height: '13%',
+    padding: 10,
+    borderRadius: 4,
+    flexDirection: 'row', // Arrange components horizontally
+  },
+  imageContainer: {
+    paddingRight: 10, // Add space to the right of the image
+  },
+
+  text: {
+    flex: 1,
+    padding: 3,
   },
 });
 
